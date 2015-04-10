4d3cd67ec7cfdf3a8d2885fe6928901a31e87cf4
Pete Nelson <pete@petenelson.com>
2015-04-10 11:47:08 -0500

    implemented per-user number of revisions in the Screen Options tab

--- includes/class-wp-revision-list-table.php
+++ includes/class-wp-revision-list-table.php
@@ -8,11 +8,20 @@ if ( !class_exists( 'WP_Revision_List_Table' ) ) {
 
 
 		public function plugins_loaded() {
+
 			add_filter( 'the_posts', array( $this, 'the_posts' ) );
 			add_filter( 'the_title', array( $this, 'the_title' ), 10, 2 );
 			add_filter( 'post_row_actions', array( $this, 'post_row_actions' ), 10, 2 );
 			add_action( 'admin_footer', array( $this, 'admin_footer' ) );
-			add_action( 'admin_init', array( $this, 'admin_init' ) );
+
+			// outputs custom screen options fields
+			add_filter( 'screen_settings', array( $this, 'screen_settings' ), 10, 2 );
+
+			// because the built-in misc/set_screen_options() does a redirect after it
+			// saves the _per_page option, this action appears to be the only way to hook
+			// into saving custom screen options
+			add_action( 'check_admin_referer', array( $this, 'save_user_meta' ), 10, 2 );
+
 		}
 
 
@@ -47,7 +56,7 @@ if ( !class_exists( 'WP_Revision_List_Table' ) ) {
 			if ( $screen->base == 'edit' && $screen->post_type == $posts[0]->post_type ) {
 
 				$new_post_list = array();
-				$revisions = $this->get_revisions_for_posts( $posts );
+				$revisions = $this->get_revisions_for_posts( $posts, $screen->post_type );
 
 				foreach ($posts as $post) {
 					$new_post_list[] = $post;
@@ -66,13 +75,26 @@ if ( !class_exists( 'WP_Revision_List_Table' ) ) {
 		}
 
 
-		private function get_revisions_for_posts( $posts ) {
-			$number_of_revisions = apply_filters( 'wp-revision-list-setting-get', 3, 'wp-revision-list-settings-general', 'number_of_revisions' );
+		private function get_user_number_of_revisions( $post_type ) {
+			$user = wp_get_current_user();
+			$revisions = get_user_meta( $user->ID, 'wp_revision_list_number_of_revisions_' . $post_type, true );
+			return $revisions === false ? $this->get_config_number_of_revisions() : intval( $revisions );
+		}
 
+
+		private function get_config_number_of_revisions() {
+			return intval( apply_filters( 'wp-revision-list-setting-get', 3, 'wp-revision-list-settings-general', 'number_of_revisions' ) );
+		}
+
+
+		private function get_revisions_for_posts( $posts, $post_type ) {
 			$revisions = array();
-			foreach( $posts as $post ) {
-				foreach ( wp_get_post_revisions( $post->ID, array( 'posts_per_page' => $number_of_revisions ) ) as $revision ) {
-					$revisions[] = new WP_Post( $revision );
+			$number_of_revisions = $this->get_user_number_of_revisions( $post_type );
+			if ( $number_of_revisions > 0 ) {
+				foreach( $posts as $post ) {
+					foreach ( wp_get_post_revisions( $post->ID, array( 'posts_per_page' => $number_of_revisions ) ) as $revision ) {
+						$revisions[] = new WP_Post( $revision );
+					}
 				}
 			}
 
@@ -102,8 +124,10 @@ if ( !class_exists( 'WP_Revision_List_Table' ) ) {
 		}
 
 
-		private function is_revision_list_screen() {
-			$screen = get_current_screen();
+		private function is_revision_list_screen( $screen = null ) {
+			if ( empty ( $screen ) ) {
+				$screen = get_current_screen();
+			}
 			return $screen->base == 'edit' && in_array( $screen->post_type, $this->get_selected_post_types() );
 		}
 
@@ -119,10 +143,78 @@ if ( !class_exists( 'WP_Revision_List_Table' ) ) {
 				</script>
 				<?php
 			}
+
 		}
-		public function admin_init() {
 
 
+		public function save_user_meta( $action, $result ) {
+
+			// see if this is coming from a screen option 'Apply'
+			// the first step in misc/set_screen_options() is check_admin_referer( 'screen-options-nonce', 'screenoptionnonce' );
+
+			$post_type = sanitize_key( filter_input( INPUT_POST, 'wp_rev_list_post_type_screen_option', FILTER_SANITIZE_STRING ) );
+
+			if ( $action === 'screen-options-nonce' && $result === 1 && ! empty( $post_type ) ) {
+
+				// yes, we're saving a screen option
+
+				// prevent a loop
+				remove_action( 'check_admin_referer', array( $this, 'save_user_meta'), 10 );
+
+				// the action and result check above came from do_action call at the end of check_admin_referer() above,
+				// so the nonce should already be valid, but we'll double-check
+				check_admin_referer( 'screen-options-nonce', 'screenoptionnonce' );
+
+				if ( ! $user = wp_get_current_user() ) {
+					return;
+				}
+
+				// make sure the post type is valid
+				if ( ! post_type_exists( $post_type ) ) {
+					return;
+				}
+
+				$set_user_revisions = intval( filter_input( INPUT_POST, 'wp_rev_list_number_of_revisions_screen_option', FILTER_SANITIZE_NUMBER_INT ) );
+
+				if ( $set_user_revisions < 0 ) {
+					$set_user_revisions = 0;
+				}
+
+				update_user_meta( $user->ID, 'wp_revision_list_number_of_revisions_' . $post_type, $set_user_revisions );
+
+			}
+
+		}
+
+
+		public function screen_settings( $screen_settings, $screen ) {
+
+			// add a field to screen options
+			if ( $this->is_revision_list_screen( $screen ) ) {
+
+				if ( ! $user = wp_get_current_user() ) {
+					return;
+				}
+
+				$number_of_revisions = $this->get_user_number_of_revisions( $screen->post_type );
+
+				ob_start();
+				?>
+				<div class="screen-options">
+					<fieldset>
+						<label for="wp_rev_list_number_of_revisions_screen_option"><?php _e( 'Number of revisions to display:', 'wp-revision-list' ); ?></label>
+							<input type="number" step="1" min="0" max="20" name="wp_rev_list_number_of_revisions_screen_option" id="wp_rev_list_number_of_revisions_screen_option" maxlength="2" value="<?php echo $number_of_revisions ?>" />
+							<input type="hidden" name="wp_rev_list_post_type_screen_option" id="wp_rev_list_post_type_screen_option" value="<?php echo esc_attr( $screen->post_type ); ?>" />
+						</label>
+					</fieldset>
+				</div>
+				<?php
+				$screen_settings = ob_get_contents();
+				ob_end_clean();
+
+			}
+
+			return $screen_settings;
 		}
 
 
